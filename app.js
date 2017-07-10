66b3f297edfdeee1445ddbddfb32a94ac3f45a2f
Jasmine Cervantes <jt24cervantes@yahoo.com>
2017-07-10 14:27:35 -0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var server = express()
 var Jasmine = require('./me.json')
+var PORT = process.env.PORT || 8080;
 
 server.get ('/', function (request, response){
   response.send("Jasmine")
@@ -10,4 +11,4 @@ server.get('/Jasmine', function (request, response) {
   response.json(Jasmine)
 })
 
-server.listen(8080)
+server.listen(PORT);
